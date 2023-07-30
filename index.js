ae4a0d6805f0a5136a162995bb89f2d805974597
skolom <nanaberimaoseiyaw@gmail.com>
2023-07-30 21:21:24 +0000

    Moved Debounce func into utils.js and added script to index.html

--- index.js
+++ index.js
@@ -10,23 +10,6 @@ const fetchData = async (searchTerm) => {
 };
 
 
-const input = document.querySelector("input");
-
-const debounce = (func, delay) => {
-    let timeoutId;
-
-    return (...args) => {
-        if (timeoutId) {        
-            clearTimeout(timeoutId);            
-        }
-        timeoutId = setTimeout(() => {
-            func.apply(null, args);
-        }, delay)
-    };
-};
-
-
-
 const onInput = event => {
   fetchData(event.target.value);     
 };
---
+++ utils.js
@@ -0,0 +1,14 @@
+const input = document.querySelector("input");
+
+const debounce = (func, delay) => {
+    let timeoutId;
+
+    return (...args) => {
+        if (timeoutId) {        
+            clearTimeout(timeoutId);            
+        }
+        timeoutId = setTimeout(() => {
+            func.apply(null, args);
+        }, delay)
+    };
+};
