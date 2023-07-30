89455d079e7b7ea6378c351bdd3217b0a1b20a95
skolom <nanaberimaoseiyaw@gmail.com>
2023-07-30 22:30:17 +0000

    iterate Movies with for of loop

--- index.js
+++ index.js
@@ -12,7 +12,15 @@ const fetchData = async (searchTerm) => {
 
 const onInput = async event => {
   const movies = await fetchData(event.target.value);   
-  console.log(movies);  
+  
+  for( let movie of movies){
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+    <img src = "${movie.Poster}" />
+    <h1> ${movie.Title} </h1>
+    `;
+  }
 };
 
 input.addEventListener('input', debounce(onInput, 1000));
