0502f313dcdfc2bcc302972f11b98e3fc3658a11
skolom <nanaberimaoseiyaw@gmail.com>
2023-07-31 17:42:08 +0000

    Created an autocomplete class to implement a dropdown-list

--- index.js
+++ index.js
@@ -14,21 +14,41 @@ const fetchData = async (searchTerm) => {
     return response.data.Search;
 };
 
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class = "input" />
+    <div class = "dropdown">
+        <div class ""dropdown-menu">
+            <div class = "dropdown-content results"><div>
+        </div>
+    </div>
+`;
+
+const input = document.querySelector('input');
+
+const dropdown = document.querySelector('.dropdown');
+
+const resultsWrapper = document.querySelector('.results');
 
 const onInput = async event => {
-  const movies = await fetchData(event.target.value);   
+  const movies = await fetchData(event.target.value); 
+  
+  dropdown.classList.add('is-active');
   
   for( let movie of movies){
-    const div = document.createElement('div');
 
-    div.innerHTML = `
+    const option = document.createElement('a');
+
+    option.classList.add('dropdown-item');
+    option.innerHTML = `
     <img src = "${movie.Poster}" />
-    <h1> ${movie.Title} </h1>
+     ${movie.Title} 
     `;
 
-    document.querySelector('#target').appendChild(div);
+    resultsWrapper.appendChild(option);
   }
   
 };
 
-input.addEventListener('input', debounce(onInput, 1000));
+input.addEventListener('input', debounce(onInput, 500));
