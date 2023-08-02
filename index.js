cad432e2bd568eb022709e6d9d23cee7e3a166ee
skolom <nanaberimaoseiyaw@gmail.com>
2023-08-02 01:19:44 +0000

    Expanded Summary and Stats

--- index.js
+++ index.js
@@ -82,5 +82,52 @@ document.addEventListener('click', event => {
         }
     });
 
-    console.log(response.data)
+    document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+  }
+
+
+  const movieTemplate = (movieDetail) => {
+    
+    // Expanded summary and Statistics
+    return `
+
+    
+    <article class ="media">
+        <figure class ="media-left">
+        <p class ="image">
+            <img src ="${movieDetail.Poster}" />
+        </p>
+        </figure>
+        <div class ="media-content"> 
+         <div class ="content">
+         <h1> ${movieDetail.Title}</h1>
+         <h4> ${movieDetail.Genre}</h4>
+         <p> ${movieDetail.Plot}</p>
+         </div>
+        </div>
+    </article>
+    
+    
+    <article class ="notification is-primary">
+        <p class = "tittle"> ${movieDetail.Awards}</p>
+        <p class ="subtitle">Awards</p>
+    </article>  
+    <article class ="notification is-primary">
+        <p class = "tittle"> ${movieDetail.BoxOffice}</p>
+        <p class ="subtitle">BoxOffice</p>
+    </article> 
+    <article class ="notification is-primary">
+        <p class = "tittle"> ${movieDetail.Metascore}</p>
+        <p class ="subtitle">Metascore</p>
+    </article> 
+    <article class ="notification is-primary">
+        <p class = "tittle"> ${movieDetail.imdbRating}</p>
+        <p class ="subtitle">IMDB Rating</p>
+    </article> 
+    <article class ="notification is-primary">
+        <p class = "tittle"> ${movieDetail.imdbVotes}</p>
+        <p class ="subtitle">IMDB Votes</p>
+    </article>   
+
+    `;
   }
